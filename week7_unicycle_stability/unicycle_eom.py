82f1ec7c0c7a9267361aa780f313e4eb2e7635e9
cinaral <alevent.cinar@gmail.com>
2020-06-28 15:40:45 +0300

    minor fix to model week7 model, added threaded animation

--- week7_unicycle_stability/unicycle_eom.py
+++ week7_unicycle_stability/unicycle_eom.py
@@ -9,13 +9,15 @@ q = sy.Matrix([th, phi])
 dq = sy.Matrix([dth, dphi])
 d2q = sy.Matrix([d2th, d2phi])
 
-T = 0.5 * mw * (dphi * sy.pi * d)**2 + 0.5 * mb * ((dphi * sy.pi * d)**2 + 2 * l * (dphi * sy.pi * d) * dth * sy.cos(th) + l**2 * dth**2) + 0.5 * 1 / (sy.pi**2 * d**2) * Iw * (dphi * sy.pi * d)**2 + 0.5 * Ib * dth**2
+wheel_vel = [dphi * d, 0] # cartesian
+body_vel = [dphi * d + l * dth * sy.cos(th), -l * dth * sy.sin(th)] # cartesian
+
+T = 0.5 * mw * (wheel_vel[0]**2 + wheel_vel[1]**2) + 0.5 * mb * (body_vel[0]**2 + body_vel[1]**2) +  0.5 * Iw * dphi**2 + 0.5 * Ib * dth**2
 V = mb * g * l * sy.cos(th)
 L = sy.Matrix([T - V])
 
 L_dq = L.jacobian(dq)
 eom = sy.simplify(L_dq.jacobian(dq) * d2q + L_dq.jacobian(q) * dq - L.jacobian(q).transpose())
-
 
 mass_matrix, RHS = sy.linear_eq_to_matrix(eom, [d2th, d2phi])
 remainder_matrix = -RHS
