82062de9e33834a2a3e53052f64ff2f65f27b23d
snsrSercan <60881491+snsrSercan@users.noreply.github.com>
2020-07-22 04:38:07 +0300

    Earhquake Frequency Response Simulation

---
+++ week12_earthquake_apartment_simulation/Game.py
@@ -0,0 +1,15 @@
+import ipycanvas as ipc
+import ipyevents as ipe
+
+from ipycanvas import Canvas
+
+from time import time, sleep
+from threading import Thread
+
+import numpy as np
+from scipy.integrate import odeint
+import sympy as sy
+
+
+
+
